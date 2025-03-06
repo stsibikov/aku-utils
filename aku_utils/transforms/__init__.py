3acc1daaf873dd2c1dcc46115e2e4514f4fe3bc8
Sergei Tsibikov <tsibikov.sergei@gmail.com>
2025-03-06 23:18:19 +0300

    redid SpecialDays, completed Lags

--- aku_utils/transforms/__init__.py
+++ aku_utils/transforms/__init__.py
@@ -12,19 +12,30 @@ import numpy as np
 class Lags(BaseEstimator, TransformerMixin):
     def __init__(
         self,
-        prt,
-        configs
+        config : List[Dict[str, Any]],
+        prt : str | List[str],
     ):
+        '''
+        Adds lag features
+
+        Parameters
+        ---
+            prt:
+                partition columns
+            configs:
+                list of dicts specifying how to generate columns
+                (see `aku_utils.transforms.configs`)
+        '''
         self.prt = prt
-        self.configs = configs
+        self.config = config
 
     def fit(self, *args):
         return self
-    
+
     def transform(self, df):
         dfat = df.assign(**{
             cf['name'] : self.transform_map[cf['t']](self, df=df, cf=cf)
-            for cf in self.configs
+            for cf in self.config
         })
         return dfat
 
@@ -32,9 +43,22 @@ class Lags(BaseEstimator, TransformerMixin):
         srs = df.groupby(self.prt)[cf['c']].shift(cf['l'])
         return srs
 
+    def mean(self, df, cf):
+        srs = df.groupby(self.prt)[cf['c']].shift(cf['l']).rolling(cf['w'], min_periods=1).mean()
+        return srs
+
+    def expmean(self, df, cf):
+        srs = (
+            df.groupby(self.prt)[cf['c']].shift(cf['l'])
+            .ewm(alpha=cf['a']).mean()
+        )
+        return srs
+
     # must be after all the transforms
     transform_map = {
-        'lag' : lag
+        'lag' : lag,
+        'mean' : mean,
+        'expmean' : expmean,
     }
 
 
@@ -42,7 +66,9 @@ class SpecialDays(BaseEstimator, TransformerMixin):
     def __init__(
         self,
         config,
-        dt_col = 'dt',
+        month = 'month',
+        day = 'day',
+        hour = 'hour',
     ):
         '''
         Adds binary features depending on whether datetime column
@@ -54,8 +80,13 @@ class SpecialDays(BaseEstimator, TransformerMixin):
                 key: name of the period, will be used as a column name
                 value: one (one date) or two (range of two dates) dates of format
                 `%m-%d %H:%M` or `%m-%d`
-            dt_col:
-                name of the datetime column
+            month:
+                name of column containing months
+            day:
+                name of column containing days
+            hour:
+                name of column containing hours
+                if data is daily, just provide any num. column
 
         Usage
         ---
@@ -75,30 +106,30 @@ class SpecialDays(BaseEstimator, TransformerMixin):
         dfat = pipeline.fit_transform(df)
         ```
         '''
-        self.dt_col = dt_col
         self.config = self.parse_config(config)
+        self.month = month
+        self.day = day
+        self.hour = hour
     
 
-    @staticmethod
-    def parse_date(v):
+    def parse_date(self, v):
         '''
-        Datetime parsing would not work because i need to know if the user specified the hour or not,
-        and datetime parsing would just return 0 in any case
+        Returns
+        ---
+            tuple with month, day, hour (may not be present) values
         '''
-        import re
-        pattern = r"^(\d{2})-(\d{2}) ?(\d{1,2})?(?::\d{2})?"
-
-        date_tuple = re.match(pattern, v)
-
-        if date_tuple is None:
-            print('invalid format. only `%m-%d %H:%M` or `%m-%d`')
-            raise ValueError
-        else:
-            date_tuple = date_tuple.groups()
-
-        date_tuple = [int(date_part) for date_part in date_tuple if date_part is not None]
-        date_dict = dict(zip(['month', 'day', 'hour'], date_tuple))
-        return date_dict
+        try:
+            date = datetime.strptime(v, '%m-%d %H:%M')
+            date_tuple = date.month, date.day, date.hour
+        except ValueError as err:
+            try:
+                date = datetime.strptime(v, '%m-%d')
+                date_tuple = date.month, date.day
+            except ValueError as err:
+                print(f'{self.__class__.__name__}: invalid date format. see docs.')
+                raise err
+        
+        return date_tuple
 
 
     def parse_config(
@@ -111,106 +142,59 @@ class SpecialDays(BaseEstimator, TransformerMixin):
             cf['name'] = name
 
             if isinstance(values, str):
-                cf['period_type'] = 'standalone'
-                date = self.parse_date(values)
-                cf['time_type'] = 'hourly' if date.get('hour') else 'daily'
-                cf['periods'] = date
+                period = self.parse_date(values)
+
+                # if only month and day was provided, we
+                # make the day into a range X 00:00 - X 23:00
+                if len(period) < 3:
+                    cf['period_type'] = 'range'
+                    start = (*period, 0)
+                    end = (*period, 23)
+                    cf['periods'] = (start, end)
+                else:
+                    cf['period_type'] = 'standalone'
+                    cf['period'] = period
             else:
                 cf['period_type'] = 'range'
                 start, end = self.parse_date(values[0]), self.parse_date(values[1])
 
-                if start.get('hour') is not None or end.get('hour') is not None:
-                    cf['time_type'] = 'hourly'
-                    start['hour'] = start.get('hour', 0)
-                    end['hour'] = end.get('hour', 23)
-                else:
-                    cf['time_type'] = 'daily'
+                if len(start) < 3:
+                    start = (*start, 0)
+                if len(end) < 3:
+                    end = (*end, 23)
                 cf['periods'] = (start, end)
-            
+
             final_config.append(cf)
         return final_config
 
     def fit(self, df, *args):
-        self.years = list(range(
-            df[self.dt_col].min().year, df[self.dt_col].max().year + 1
-        ))
-
-        self.config = [
-            self.add_specific_periods(config, years=self.years)
-            for config in self.config
-        ]
-
         return self
 
-
-    def add_specific_periods(
-        self,
-        cf : Dict[str, Any],
-        years : List[int],
-    ) -> Dict[str, Any]:
-        '''
-        Modifies original config
-        '''
-        dt_format = '%Y-%m-%d'
-        if cf['time_type'] == 'hourly':
-            dt_format += ' %H:%M'
-
-        if cf['period_type'] == 'range':
-            start = cf['periods'][0]
-            end = cf['periods'][1]
-            next_year_reach = (
-                (start['month'], start['day']) >
-                (end['month'], end['day'])
-            )
-            cf['periods'] = [
-                (
-                    datetime(
-                        year=year, month=start['month'], day=start['day'], hour=start.get('hour', 0)
-                    ).strftime(dt_format),
-                    datetime(
-                        year=year+int(next_year_reach), month=end['month'], day=end['day'], hour=end.get('hour', 0)
-                    ).strftime(dt_format),
-                )
-                for year in years
-            ]
-        else:
-            period = cf['periods']
-            cf['periods'] = [
-                datetime(
-                    year=year, month=period['month'], day=period['day'], hour=period.get('hour', 0)
-                ).strftime(dt_format)
-                for year in years
-            ]
-        return cf
-
-
     def transform(self, df):
-        dfat = df.assign(**{
-            config['name'] : self.apply_period(df, config)
+        # date tuple col alias
+        dtc = f'{self.__class__.__name__} temp'
+
+        df[dtc] = list(zip(df['obj_id'], df['target'], df['hour']))
+        self.date_tuple_col = df[dtc]
+
+        df = df.assign(**{
+            config['name'] : self.is_in_period(df, config)
             for config in self.config
         })
-        return dfat
 
-    def apply_period(self, df, cf):
-        binary_array : pd.Series = None
-
-        dt_srs = (
-            df['dt'] if cf['time_type'] == 'hourly'
-            else df['dt'].dt.normalize()
-        )
-
-        # period - str or tuple of str
-        for period in cf['periods']:
-            # this binary array
-            tba = (
-                dt_srs == period if cf['period_type'] == 'standalone'
-                else dt_srs.between(period[0], period[1])
-            )
-
-            # combining filters
-            binary_array = (
-                tba if binary_array is None
-                else binary_array | tba
-            )
-        binary_array = binary_array.astype('int')
-        return binary_array
+        df = df.drop(columns=dtc)
+        return df
+    
+    def is_in_period(self, df, cf):
+        if cf['period_type'] == 'range':
+            start, end = cf['periods']
+            if start <= end:
+                srs = (start <= self.date_tuple_col) & (self.date_tuple_col <= end)
+            # If the range spans into the next year (e.g., Dec -> Jan)
+            else:    
+                srs = (self.date_tuple_col >= start) | (self.date_tuple_col <= end)
+        else:
+            srs = self.date_tuple_col == cf['period']
+        
+        srs = srs.astype('int')
+        return srs
